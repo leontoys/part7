1b83284646e610af6545dcda30f7aab394100ea8
leontoys <svliyon@gmail.com>
2025-03-22 06:39:54 +1100

    7.10

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef,  useReducer } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 import BlogForm from './components/BlogForm'
+import { useQuery } from '@tanstack/react-query'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -13,6 +14,61 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [message, setMessage] = useState({ message: null, className: null })
+
+  //useReducer
+  const notificatinReducer = (state,action) => {
+    switch(action.type){
+    case 'ERROR_LOGIN' :
+      return {
+        message: 'user name or password is wrong',
+        className : 'error'
+      }
+    case 'ERROR_LOGOUT' :
+      return {
+        message: 'logout failed',
+        className : 'error'
+      }
+    case 'SUCCESS_ADDBLOG' :
+      return {
+        message: action.payload,
+        className : 'notification'
+      }
+    case 'ERROR_ADDBLOG':
+      return{
+        message : 'error creating blog',
+        className : 'error'
+      }
+    case 'SUCCESS_UPDATEBLOG':
+      return{
+        message : 'liked blog',
+        className : 'error'
+      }
+    case 'ERROR_UPDATEBLOG':
+      return{
+        message : 'error liking blog',
+        className : 'error'
+      }
+    case 'SUCCESS_DELETEBLOG' :
+      return {
+        message: 'successfully deleted blog',
+        className : 'notification'
+      }
+    case 'ERROR_DELETEBLOG':
+      return{
+        message : 'error deleting blog',
+        className : 'error'
+      }
+    case 'CLEAR' :
+      return {
+        message: '',
+        className : ''
+      }
+    default:
+      state
+    }
+  }
+
+  const [notification,notificationDispatch] = useReducer(notificatinReducer,{ message:'',className:'' })
   //reference
   const blogFormRef = useRef()
 
@@ -55,18 +111,10 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      const newMessage = {
-        message: 'user name or password is wrong',
-        className: 'error',
-      }
-      setMessage(newMessage)
-      setTimeout(() => {
-        const newMessage = {
-          message: null,
-          className: null,
-        }
-        setMessage(newMessage)
-      }, 5000)
+
+      notificationDispatch({ type : 'ERROR_LOGIN' })
+      setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
+
     }
   }
 
@@ -77,18 +125,10 @@ const App = () => {
       window.localStorage.clear()
       setUser(null)
     } catch (exception) {
-      const newMessage = {
-        message: 'log out failed',
-        className: 'error',
-      }
-      setMessage(newMessage)
-      setTimeout(() => {
-        const newMessage = {
-          message: null,
-          className: null,
-        }
-        setMessage(newMessage)
-      }, 5000)
+
+      notificationDispatch({ type : 'ERROR_LOGOUT' })
+      setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
+
     }
   }
 
@@ -97,32 +137,15 @@ const App = () => {
     try {
       blogService.create(blogObject).then((returnedBlog) => {
         setBlogs(blogs.concat(returnedBlog))
-        const newMessage = {
-          message: `a new blog ${blogObject.title} by ${blogObject.author} added`,
-          className: 'notification',
-        }
-        setMessage(newMessage)
-        setTimeout(() => {
-          const newMessage = {
-            message: null,
-            className: null,
-          }
-          setMessage(newMessage)
-        }, 5000)
+
+        notificationDispatch({ type : 'SUCCESS_ADDBLOG' })
+        setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
+
       })
     } catch (exception) {
-      const newMessage = {
-        message: 'error creating blog',
-        className: 'error',
-      }
-      setMessage(newMessage)
-      setTimeout(() => {
-        const newMessage = {
-          message: null,
-          className: null,
-        }
-        setMessage(newMessage)
-      }, 5000)
+
+      notificationDispatch({ type : 'ERROR_ADDBLOG' })
+      setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
     }
   }
 
@@ -137,18 +160,9 @@ const App = () => {
         setBlogs(updatedBlogs)
       })
     } catch (exception) {
-      const newMessage = {
-        message: 'error liking blog',
-        className: 'error',
-      }
-      setMessage(newMessage)
-      setTimeout(() => {
-        const newMessage = {
-          message: null,
-          className: null,
-        }
-        setMessage(newMessage)
-      }, 5000)
+      notificationDispatch({ type : 'ERROR_UPDATEBLOG' })
+      setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
+
     }
   }
 
@@ -160,34 +174,14 @@ const App = () => {
         const updatedBlogs = blogs.filter((blog) => blog.id !== blogId)
         setBlogs(updatedBlogs)
 
-        // Optionally set a success message
-        const newMessage = {
-          message: 'Blog deleted successfully',
-          className: 'success',
-        }
-        setMessage(newMessage)
-        setTimeout(() => {
-          const newMessage = {
-            message: null,
-            className: null,
-          }
-          setMessage(newMessage)
-        }, 5000)
+        notificationDispatch({ type : 'SUCCESS_DELETEBLOG' })
+        setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
       })
     } catch (exception) {
-      // Set error message in case of an exception
-      const newMessage = {
-        message: 'Error deleting blog',
-        className: 'error',
-      }
-      setMessage(newMessage)
-      setTimeout(() => {
-        const newMessage = {
-          message: null,
-          className: null,
-        }
-        setMessage(newMessage)
-      }, 5000)
+
+      notificationDispatch({ type : 'ERROR_DELETEBLOG' })
+      setTimeout(() => notificationDispatch({ type : 'CLEAR' }), 5000)
+
     }
   }
 
@@ -250,7 +244,7 @@ const App = () => {
 
   return (
     <div>
-      <Notification message={message.message} className={message.className} />
+      <Notification message={notification.message} className={notification.className} />
       {!user && loginForm()}
       {user && blogForm()}
     </div>
